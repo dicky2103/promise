d069fd9d5f5b65156e328295f59c8d9c02a2225a
dicky azhari hernuwan <hernuwandicky@gmail.com>
2022-08-07 20:06:05 +0700

    promise

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import app from './component/app';
+
+import 'bootstrap/disct/css/bootstrap.css';
+import 'bootstrap/dist/css/js/bootstrap.js';
+import = as serviceWorker from './serviceWorker';
---
+++ src/component/grid.jsx
@@ -0,0 +1,26 @@
+import React, {component} from 'react';
+class Grid extends Component{
+
+    render(){
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3 col-6 test">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, in.
+                    </div>
+                    <div className="col-md-3 col-6 test">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, in.
+                    </div>
+                    <div className="col-md-3 col-6 test">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, in.
+                    </div>
+                    <div className="col-md-3 col-6 test">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, in.
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Grid;
